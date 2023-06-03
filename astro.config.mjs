4a4bf57ad07b2e450869eb6d7e8b45e93807a899
kion <kion@dashgl.com>
2023-06-04 04:40:24 +0900

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://dashgl.github.io',
+	base: '/.github',
 	integrations: [mdx(), sitemap()],
 });
